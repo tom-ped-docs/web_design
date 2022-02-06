470d9e6e4142735e37e9b873afb53f37e1eca95e
Tomasz <tom.ped.docs@gmail.com>
2022-02-06 22:47:45 +0100

    message

---
+++ fluent_design_dystem/index.js
@@ -0,0 +1,21 @@
+// Bootstrap Docs / Components / Tooltips https://getbootstrap.com/docs/5.0/components/tooltips/
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Bootstrap Docs / Components / Toasts https://getbootstrap.com/docs/5.0/components/toasts/
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
+
+const BUTTON = document.getElementById('button');
+const TOAST = document.getElementById('toast');
+
+BUTTON.addEventListener('click', function () {
+  var a = new bootstrap.Toast(TOAST);
+  a.show();
+});
